6d6f2b9a75f3a52927046b57473d1d7bc2e5797f
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-03-17 13:55:03 +0100

    change seeding and some parameters for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,19 @@ using Distributions
 using Test
 using Random
 
-@testset "Univariate continuous Mixture" begin
+
+@testset "Univariate continuous Mixture Exponential + Gamma" begin
+    Random.seed!(1234)
     N = 50_000
-    seed = MersenneTwister(0)
     θ₁ = 10
     θ₂ = 5
-    α = 0.5
-    β = 0.3
+    α = 0.8
+    β = 0.6
     rtol = 6e-2
     mix_true = MixtureModel([Exponential(θ₁), Gamma(α, θ₂)], [β, 1 - β])
-    y = rand(seed, mix_true, N)
+    y = rand(mix_true, N)
     mix_guess = MixtureModel([Exponential(1), Gamma(0.5, 1)], [0.5, 1 - 0.5])
-    mix_mle = fit_mle(mix_guess, y; display=:none, tol=1e-3, robust=false, infos=false)
+    mix_mle = fit_mle(mix_guess, y; display=:none, atol=1e-3, robust=false, infos=false)
 
     p = params(mix_mle)[1]
     @test isapprox([β, 1 - β], probs(mix_mle); rtol=rtol)
@@ -23,9 +24,31 @@ using Random
     @test isapprox(θ₂, p[2][2]; rtol=rtol)
 end
 
-@testset "Multivariate Gaussian Mixture" begin
+@testset "Stochastic EM Univariate continuous Mixture Exponential + Laplace" begin
+    Random.seed!(1234)
     N = 50_000
-    seed = MersenneTwister(0)
+    θ₁ = 10
+    θ₂ = 0.8
+    α = 0.5
+    β = 0.3
+    μ = -1
+    rtol = 6e-2
+    mix_true = MixtureModel([Laplace(μ, θ₁), Normal(α, θ₂)], [β, 1 - β])
+    y = rand(mix_true, N)
+    mix_guess = MixtureModel([Laplace(1), Normal(0.5, 1)], [0.5, 1 - 0.5])
+    mix_mle = fit_mle(mix_guess, y; display=:none, atol=1e-3, robust=false, infos=false, method = StochasticEM())
+
+    p = params(mix_mle)[1]
+    @test isapprox([β, 1 - β], probs(mix_mle); rtol=rtol)
+    @test isapprox(θ₁, p[1][2]; rtol=rtol)
+    @test isapprox(μ, p[1][1]; rtol=rtol)
+    @test isapprox(α, p[2][1]; rtol=rtol)
+    @test isapprox(θ₂, p[2][2]; rtol=rtol)
+end
+
+@testset "Multivariate Gaussian Mixture" begin
+    Random.seed!(1234)
+    N = 50_000
     rtol = 5e-2
     θ₁ = [-1, 1]
     θ₂ = [0, 2]
@@ -40,7 +63,7 @@ end
     mix_true = MixtureModel([D₁, D₂], [β, 1 - β])
 
     # Generate samples from the true distribution
-    y = rand(seed, mix_true, N)
+    y = rand(mix_true, N)
 
     # Initial Condition
     D₁guess = MvNormal([0.2, 1], [1 0.6; 0.6 1])
@@ -48,7 +71,7 @@ end
     mix_guess = MixtureModel([D₁guess, D₂guess], [0.4, 0.6])
 
     # Fit MLE
-    mix_mle = fit_mle(mix_guess, y; display=:none, tol=1e-3, robust=false, infos=false)
+    mix_mle = fit_mle(mix_guess, y; display=:none, atol=1e-3, robust=false, infos=false)
 
     p = params(mix_mle)[1]
     @test isapprox([β, 1 - β], probs(mix_mle); rtol=rtol)
@@ -58,8 +81,8 @@ end
 
 # Bernoulli Mixture i.e. Mixture of Bernoulli Product (S = 10 term and K = 3 mixture components).
 @testset "Multivariate Product Bernoulli Mixture" begin
+    Random.seed!(1234)
     N = 50_000
-    seed = MersenneTwister(0)
     rtol = 5e-2
 
     S = 10
@@ -73,13 +96,13 @@ end
     mix_true = MixtureModel([product_distribution(Bernoulli.(θ[:, i])) for i in 1:K], [β / 2, 1 - β, β / 2])
 
     # Generate samples from the true distribution
-    y = rand(seed, mix_true, N)
+    y = rand(mix_true, N)
 
     # Initial Condition
     mix_guess = MixtureModel([product_distribution(Bernoulli.(2θ[:, i] / 3)) for i in 1:K], [0.25, 0.55, 0.2])
 
     # Fit MLE
-    mix_mle = fit_mle(mix_guess, y; display=:none, tol=1e-3, robust=false, infos=false)
+    mix_mle = fit_mle(mix_guess, y; display=:none, atol=1e-3, robust=false, infos=false)
 
     p = params(mix_mle)[1]
     @test isapprox([β / 2, 1 - β, β / 2], probs(mix_mle); rtol=rtol)
@@ -87,8 +110,8 @@ end
 end
 
 @testset "Univariate continuous Mixture of (mixture + Normal)" begin
+    Random.seed!(1234)
     N = 50_000
-    seed = MersenneTwister(0)
     θ₁ = -5
     θ₂ = 2
     σ₁ = 1
@@ -103,7 +126,7 @@ end
     d1 = MixtureModel([Normal(θ₁, σ₁), Normal(θ₂, σ₂)], [α, 1 - α])
     d2 = Normal(θ₀, σ₀)
     mix_true = MixtureModel([d1, d2], [β, 1 - β])
-    y = rand(seed, mix_true, N)
+    y = rand(mix_true, N)
 
     # We choose initial guess very close to the true solution just to show the EM algorithm convergence.
     # This particular choice of mixture of mixture Gaussian with another Gaussian is non identifiable hence we execpt other solution far away from the true solution
@@ -111,8 +134,8 @@ end
     d2_guess = Normal(θ₀ + 0.1, σ₀ - 0.01)
 
     mix_guess = MixtureModel([d1_guess, d2_guess], [β + 0.1, 1 - β - 0.1])
-    mix_mle = fit_mle(mix_guess, y; display=:none, tol=1e-3, robust=false, infos=false)
-    y_guess = rand(seed, mix_mle, N)
+    mix_mle = fit_mle(mix_guess, y; display=:none, atol=1e-3, robust=false, infos=false)
+    y_guess = rand(mix_mle, N)
 
     @test probs(mix_mle) ≈ [β, 1 - β] rtol = rtol
     p = params(mix_mle)[1]
@@ -125,9 +148,8 @@ end
     @test σ₀ ≈ p[2][2] rtol = rtol
 end
 
-@testset "Univariate continuous Mixture of (mixture + Normal)" begin
+@testset "Univariate continuous Mixture of (Laplace + Normal)" begin
     N = 50_000
-    seed = MersenneTwister(0)
     θ₁ = -2
     θ₂ = 2
     σ₁ = 1
@@ -142,7 +164,7 @@ end
     d1 = MixtureModel([Normal(θ₁, σ₁), Laplace(θ₂, σ₂)], [α, 1 - α])
     d2 = Normal(θ₀, σ₀)
     mix_true = MixtureModel([d1, d2], [β, 1 - β])
-    y = rand(seed, mix_true, N)
+    y = rand(mix_true, N)
 
     # We choose initial guess very close to the true solution just to show the EM algorithm convergence.
     # This particular choice of mixture of mixture Gaussian with another Gaussian is non identifiable hence we execpt other solution far away from the true solution
@@ -150,11 +172,11 @@ end
     d2_guess = Normal(θ₀ + 2, 10σ₀)
 
     mix_guess = MixtureModel([d1_guess, d2_guess], [β + 0.1, 1 - β - 0.1])
-    mix_mle = fit_mle(mix_guess, y; display=:none, tol=1e-3, robust=false, infos=false)
+    mix_mle = fit_mle(mix_guess, y; display=:none, atol=1e-3, robust=false, infos=false)
     # without print
     # 1.368 s (17002715 allocations: 1.48 GiB)
     #  1.485 s (17853393 allocations: 1.61 GiB)
-    y_guess = rand(seed, mix_mle, N)
+    y_guess = rand(mix_mle, N)
 
     @test probs(mix_mle) ≈ [β, 1 - β] rtol = rtol
     p = params(mix_mle)[1]
@@ -165,4 +187,20 @@ end
     @test σ₂ ≈ p[1][1][2][2] rtol = rtol
     @test θ₀ ≈ p[2][1] rtol = rtol
     @test σ₀ ≈ p[2][2] rtol = rtol
+end
+
+@testset "Most likely category identification" begin
+    Random.seed!(1234)
+    m=MixtureModel([Normal(), Laplace(2)], [0.2, 0.8])
+    α = probs(m)
+    dists = components(m)
+    N = 1000
+    z = zeros(Int, N)
+    y = zeros(N)
+    for i in 1:N 
+        z[i] = rand(Categorical(α))
+        y[i] = rand(dists[z[i]])
+    end
+    ẑ = most_likely_cat(m, y)
+    @test count(ẑ .== z)/N > 0.85
 end
