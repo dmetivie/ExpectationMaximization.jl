7016091cf8cddb2475f27d398b672f9e2549f32a
David Métivier <46794064+dmetivie@users.noreply.github.com>
2025-07-03 23:16:04 +0200

    favicon path

--- docs/make.jl
+++ docs/make.jl
@@ -4,12 +4,17 @@ using Distributions, StatsPlots
 include("pages.jl")
 
 makedocs(
-    sitename = "ExpectationMaximization.jl",
-    authors = "David Métivier",
-    modules = [ExpectationMaximization],
-    clean = true,
-    doctest = false,
-    pages = pages,
+    sitename="ExpectationMaximization.jl",
+    authors="David Métivier",
+    modules=[ExpectationMaximization],
+    clean=true,
+    doctest=false,
+    assets=["assets/favicon.ico"],
+    format=Documenter.HTML(assets=["assets/favicon.ico"],
+        prettyurls=true,
+        repolink="https://github.com/dmetivie/ExpectationMaximization.jl",
+        canonical="https://dmetivie.github.io/ExpectationMaximization.jl",),
+    pages=pages,
 )
 
-deploydocs(repo = "github.com/dmetivie/ExpectationMaximization.jl.git")
+deploydocs(repo="github.com/dmetivie/ExpectationMaximization.jl.git")
